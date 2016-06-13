76ac67499e2c397ac0ac99e1b9212dc6deb9553b
ArthurLuz <arthurluz17@uft.edu.br>
2016-06-13 13:42:05 -0300

    Projex Sol Iniciado

---
+++ src/java/br/com/uft/projexsol/application/beans/Voluntario.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.uft.projexsol.application.beans;
+
+/**
+ *
+ * @author aluno
+ */
+
+public class Voluntario {
+    private String nome;
+    private String codigo;
+    private String cpf;
+    private String rg;
+    private String login;
+    private String senha;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+}
